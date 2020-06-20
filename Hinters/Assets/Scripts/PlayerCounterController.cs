146c57ca5a98c503bccb223200e744add4e8c846
Hans Arijanto <hansarijanto@hotmail.com>
2020-06-19 20:39:53 -0400

    init player controller script

---
+++ Hinters/Assets/Scripts/PlayerCounterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCounterController : MonoBehaviour
+{
+  // public TextMestProUGUI
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Debug.Log("BEFORE BLAH");
+    Debug.Log("BLAH");
+  }
+}
